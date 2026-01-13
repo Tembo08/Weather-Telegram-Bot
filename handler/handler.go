a733f9be7e022005ed291f9a570901b8969bc93a
Tembo08 <romasaharok775@gmail.com>
2026-01-14 00:09:45 +0300

    добавил функции: SetCity,SendWeather,UnknownCommand в handler -e

--- handler/handler.go
+++ handler/handler.go
@@ -42,25 +42,39 @@ func (h *Handler) handleUpdate(update tgbotapi.Update) {
 	if update.Message.IsCommand() {
 		switch update.Message.Command() {
 		case "city":
-			city := update.Message.CommandArguments()
-			h.userCities[update.Message.From.ID] = city
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Город %s сохранен", city))
-			msg.ReplyToMessageID = update.Message.MessageID
-			h.bot.Send(msg)
+			h.handleSetCity(update)
+			return
+		case "weather":
+			h.handleSendWeather(update)
 			return
 		default:
-			log.Printf("New comand [%s] %s", update.Message.From.UserName, update.Message.Text)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Такая команда не доступна")
-			msg.ReplyToMessageID = update.Message.MessageID
-			h.bot.Send(msg)
+			h.handleUnknownCommand(update)
 			return
 		}
 	}
+}
 
-	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-	coordinates, err := h.owClient.Coordinates(update.Message.Text)
+func (h Handler) handleSetCity(update tgbotapi.Update) {
+	city := update.Message.CommandArguments()
+	h.userCities[update.Message.From.ID] = city
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Город %s сохранен", city))
+	msg.ReplyToMessageID = update.Message.MessageID
+	h.bot.Send(msg)
+
+}
+
+func (h *Handler) handleSendWeather(update tgbotapi.Update) {
+	city, ok := h.userCities[update.Message.From.ID]
+	if !ok {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Сначала установите город с помощью команды /city\nВот так '/city Москва'")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+
+	coordinates, err := h.owClient.Coordinates(city)
 	if err != nil {
-		log.Println(err)
+		log.Printf("error owClient.Coordinates: %v", err)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить координаты")
 		msg.ReplyToMessageID = update.Message.MessageID
 		h.bot.Send(msg)
@@ -69,7 +83,7 @@ func (h *Handler) handleUpdate(update tgbotapi.Update) {
 
 	weather, err := h.owClient.Weather(coordinates.Lat, coordinates.Lon)
 	if err != nil {
-		log.Println(err)
+		log.Printf("error owClient.Weather: %v", err)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить погоду в этой местности")
 		msg.ReplyToMessageID = update.Message.MessageID
 		h.bot.Send(msg)
@@ -78,10 +92,17 @@ func (h *Handler) handleUpdate(update tgbotapi.Update) {
 
 	msg := tgbotapi.NewMessage(
 		update.Message.Chat.ID,
-		fmt.Sprintf("Температура в %s: %d°C", update.Message.Text, int(math.Round(weather.Temp))),
+		fmt.Sprintf("Температура в %s: %d°C", city, int(math.Round(weather.Temp))),
 	)
 	msg.ReplyToMessageID = update.Message.MessageID
 
 	h.bot.Send(msg)
 
+}
+
+func (h *Handler) handleUnknownCommand(update tgbotapi.Update) {
+	log.Printf("Unrnown command [%s] %s", update.Message.From.UserName, update.Message.Text)
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Такая команда не доступна")
+	msg.ReplyToMessageID = update.Message.MessageID
+	h.bot.Send(msg)
 }
